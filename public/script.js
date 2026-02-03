dca78f2467330acec41e269a2af4e92a36f24af8
Sbee8777 <seb.bastiaans@hva.nl>
2026-02-03 15:03:35 +0100

    HTML en CSS rebuild

---
+++ public/script.js
@@ -0,0 +1,6 @@
+// let mouse = document.querySelector('img');
+
+// mouse.addEventListener("mousemove", function (move) {
+//   mouse.style.setProperty("--y", move.offsetY + "px");
+//   mouse.style.setProperty("--x", move.offsetX + "px");
+// });
