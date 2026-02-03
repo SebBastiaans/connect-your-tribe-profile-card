6ee189d21340978e1c1fb2c229875609ea4f8e9b
Sbee8777 <seb.bastiaans@hva.nl>
2026-02-03 17:45:18 +0100

    Hover gradient

--- public/script.js
+++ public/script.js
@@ -1,6 +1,6 @@
-// let mouse = document.querySelector('img');
+let mouse = document.querySelector('img');
 
-// mouse.addEventListener("mousemove", function (move) {
-//   mouse.style.setProperty("--y", move.offsetY + "px");
-//   mouse.style.setProperty("--x", move.offsetX + "px");
-// });
+mouse.addEventListener("mousemove", function (move) {
+  mouse.style.setProperty("--y", move.offsetY + "px");
+  mouse.style.setProperty("--x", move.offsetX + "px");
+});
